87b0b79f2102b8d6793a44abf81248647beb64e1
dianaOli <deolivas4@miners.utep.edu>
2024-03-06 10:12:32 -0700

    got rid of hardcoded code

--- wordCount.py
+++ wordCount.py
@@ -1,38 +1,35 @@
-#! /usr/bin/env python3.12
+#! /usr/bin/env python3
 
 import os
 import re
+import sys
 
-input = "C:\\Users\\Diana\\Desktop\\2024 spring\\os\\python-intro-dianaOli\\input.txt"
-output = "C:\\Users\\Diana\\Desktop\\2024 spring\os\\python-intro-dianaOli\\output.txt"
+# Check if the correct number of command-line arguments is provided
+if len(sys.argv) != 3:
+    print("Usage: python script.py input_file output_file")
+    sys.exit(1)
 
-#check if file exist
-if os.path.exists(input):
+textFile = sys.argv[1]
+outputFile = sys.argv[2]
+
+# Check if the input file exists
+if os.path.exists(textFile):
     # 'r' mode used, file open to read
-    with open(input, 'r') as file:
-        #reading the file 
+    with open(textFile, 'r') as file:
+        # reading the file
         file_words = file.read()
-        #writing bytes to file descriptor, encode() converting string to bytes
-        #fancy print 
-        #os.write(1,file_words.encode())
+
     words = re.findall(r'\b\w+\b', file_words.lower())
-    #add words to a list
-    #words_list = file_words.split()
-    #dictionary
     word_count = {}
     for word in words:
-       
-        
-        #filling dictionary and counting words 
-        word_count[word] = word_count.get(word,0)+1
+        # filling dictionary and counting words
+        word_count[word] = word_count.get(word, 0) + 1
 
     sorted_word_count = dict(sorted(word_count.items()))
-    #file open to appending and writing
-    with open(output, 'a+') as out:
 
+    # file open to appending and writing
+    with open(outputFile, 'a+') as out:
         for word, count in sorted_word_count.items():
-            out.write(f"{word}: {count}\n")
-
-    
+            out.write(f"{word} {count}\n")
 else:
-    print("sorry i didn't find a text file,check path")
+    print("Sorry, the input file does not exist or the path is incorrect.")
